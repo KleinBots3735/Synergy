535d1630850abb91910c30091310923eb7507ea8
Rohan Koripalli <rohan@koripalli.com>
2014-10-07 19:51:58 -0500

    Added Ports to RobotMap.java

--- src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -1,5 +1,10 @@
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -7,13 +12,43 @@ package edu.wpi.first.wpilibj.templates;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static final int leftMotor = 1;
-    // public static final int rightMotor = 2;
     
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static final int rangefinderPort = 1;
-    // public static final int rangefinderModule = 1;
+//PORT MAP
+//PORTS ON DRIVER STATION LAPTOP
+    static final int PILOTSTICK_PORT = 1; //P880 Controller
+    static final int COPILOTSTICK_PORT = 2;    //Logitech Controller
+//PORTS ON DIGITAL SIDECAR
+    //PWM PORTS
+        static final int FRONTLEFTMOTOR_PORT = 1;
+        static final int FRONTRIGHTMOTOR_PORT = 2;
+        static final int REARLEFTMOTOR_PORT = 3;
+        static final int REARRIGHTMOTOR_PORT = 4;
+        static final int ARMMOTOR_PORT = 5;
+    //DIGITAL PORTS
+        static final int PRESSURESWITCH_PORT = 1;
+    //RELAY PORTS
+        static final int COMPRESSORSPIKE_PORT = 1;
+        
+//PORTS ON CRIO
+    //ANALOG PORTS (BREAKOUT #1)
+    //SOLENOID PORTS (BREAKOUT #3)
+        static final int SHIFTERSOLENOID_PORT = 1;
+        static final int ARMSOLENOID_PORT = 2;
+
+        
+//COMPONENTS ON DRIVER STATION LAPTOP
+    public static Joystick pilotStick = new Joystick(PILOTSTICK_PORT);
+    public static Joystick coPilotStick = new Joystick(COPILOTSTICK_PORT);
+    
+//COMPONENTS ON ROBOT
+    public static Talon frontLeftMotor = new Talon(FRONTLEFTMOTOR_PORT);   //PWM #1
+    public static Talon frontRightMotor = new Talon(FRONTRIGHTMOTOR_PORT);  //PWM #2
+    public static Talon rearLeftMotor = new Talon(REARLEFTMOTOR_PORT);    //PWM #3
+    public static Talon rearRightMotor = new Talon(REARRIGHTMOTOR_PORT);   //PWM #4
+    public static Talon armMotor = new Talon(ARMMOTOR_PORT);   //PWM #4
+    
+    public static Compressor compressor = new Compressor(PRESSURESWITCH_PORT,COMPRESSORSPIKE_PORT);
+    
+    public static Solenoid arm = new Solenoid(ARMSOLENOID_PORT);
+    public static Solenoid shifter = new Solenoid(SHIFTERSOLENOID_PORT);
 }
